da7b7cb668095a0dd74dc212109412d84f9e62c1
Guilherme Oliveira Loiola <52641472+guioliunb@users.noreply.github.com>
2022-07-27 09:53:06 -0300

    Read input, Parse commands, Save history.

---
+++ HelloShell.h
@@ -0,0 +1,14 @@
+#define ARG_MAX_COUNT    1024      /* max number of arguments to a command */
+#define HISTORY_MAXITEMS 100 
+
+struct command {		   /* a struct that stores a parsed command */
+	int argc;                  /* number of arguments in the comand */
+	char *name;                /* name of the command */
+	char *argv[ARG_MAX_COUNT]; /* the arguments themselves */
+	int fds[2];                /* the file descriptors for input/output */
+};
+
+struct commands {                  /* struct to store a command pipeline */
+	int cmd_count;             /* number of commands in the pipeline */
+	struct command *cmds[];    /* the commands themselves */
+};
